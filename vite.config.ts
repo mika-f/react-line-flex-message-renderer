ef760d253f7c6803c9535c68253c9f3da20cfa9a
mrtska <mrtska.starring@gmail.com>
2026-01-16 21:23:57 +0900

    Remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     dts({
       include: ["src/**/*"],
       exclude: ["src/**/*.stories.{ts,tsx}"],
