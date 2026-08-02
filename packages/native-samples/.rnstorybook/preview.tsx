6bfa12eef00389a79ab5319a334f6d89a18480da
Natsune Mochizuki <me@natsuneko.cat>
2026-08-02 14:00:42 +0900

    chore(native-saimples): set bg color

--- packages/native-samples/.rnstorybook/preview.tsx
+++ packages/native-samples/.rnstorybook/preview.tsx
@@ -1,6 +1,18 @@
 import type { Preview } from '@storybook/react-native';
+import { StyleSheet, View } from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#849ebf',
+    width: '100%',
+    height: '100%',
+  }
+});
 
 const preview: Preview = {
+  decorators: [
+    (story) => <View style={Styles.container}>{story()}</View>
+  ],
   parameters: {
     controls: {
       matchers: {
